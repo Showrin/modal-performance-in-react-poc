c2a384e208771a462a52e54e55deacd2e4aaef61
Showrin Barua <showrinece15@gmail.com>
2023-04-03 08:53:52 +0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { useState } from "react";
+import { Button, Checkbox } from "react-bootstrap";
+import ModalBody from "./components/ModalBody";
+import CustomModal from "./components/CustomModal";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles.css";
+
+export default function App() {
+  const [shouldShowModal, setShouldShowModal] = useState(false);
+  const [shouldDestroyOnClose, setShouldDestroyOnClose] = useState(true);
+
+  const onClickShowModalButton = () => {
+    setShouldShowModal(true);
+  };
+
+  const handleModalClose = () => {
+    setShouldShowModal(false);
+  };
+
+  return (
+    <div className="App">
+      <h1>Modal Performance R&D</h1>
+
+      <div>
+        shouldDestroyOnClose?
+        <Checkbox
+          checked={shouldDestroyOnClose}
+          onChange={() => setShouldDestroyOnClose((prevState) => !prevState)}
+        />
+      </div>
+
+      <Button onClick={onClickShowModalButton}>Show Modal</Button>
+
+      <CustomModal
+        shouldShow={shouldShowModal}
+        handleClose={handleModalClose}
+        shouldDestroyOnClose={shouldDestroyOnClose}
+      >
+        <ModalBody />
+      </CustomModal>
+    </div>
+  );
+}
---
+++ src/components/CustomModal.jsx
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from "react";
+import { Modal, Button } from "react-bootstrap";
+import classNames from "classnames";
+
+const CustomModal = (props) => {
+  const { shouldShow, handleClose, shouldDestroyOnClose, children } = props;
+  const [shouldOpen, setShouldOpen] = useState(false);
+
+  useEffect(() => {
+    if (shouldShow && !shouldOpen) {
+      setShouldOpen(true);
+    }
+
+    if (!shouldShow && shouldDestroyOnClose) {
+      setShouldOpen(false);
+    }
+
+    if (!shouldDestroyOnClose) {
+      if (shouldShow) {
+        document.body.style.overflow = "hidden";
+        document.body.style.paddingRight = "17px";
+        document.body.classList.add("modal-open");
+      } else {
+        document.body.style.overflow = "auto";
+        document.body.style.paddingRight = 0;
+        document.body.classList.remove("modal-open");
+      }
+    }
+  }, [shouldOpen, shouldShow, shouldDestroyOnClose]);
+
+  return (
+    <Modal
+      className={classNames("test-modal", {
+        "test-modal--hide": !shouldShow
+      })}
+      backdropClassName={classNames("test-modal__backdrop", {
+        "test-modal__backdrop--hide": !shouldShow
+      })}
+      show={shouldOpen}
+      onHide={handleClose}
+    >
+      <Modal.Header closeButton>
+        <Modal.Title>Modal heading</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>{children}</Modal.Body>
+      <Modal.Footer>
+        <Button onClick={handleClose}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+CustomModal.defaultProps = {
+  shouldDestroyOnClose: false
+};
+
+export default CustomModal;
